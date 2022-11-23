bcc3f7006109cc1b6a03ad6b890071222f22416d
JoHyunwoo <jhw3874@gmail.com>
2022-11-23 23:27:24 +0900

    회원 서비스 테스트 케이스

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -9,7 +9,11 @@ import java.util.List;
 import java.util.Optional;
 @Service
 public class MemberService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+    //DI
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /**
      * 회원 가입
