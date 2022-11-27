a03f414a2333dd77fe8972604e6431a94a11a8e6
JoHyunwoo <jhw3874@gmail.com>
2022-11-28 02:44:47 +0900

    스프링 JdbcTemplate > JPA(Java Persistence API)

--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,6 +1,14 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity //JPA를 사용하기 위해서는 엔티티를 매핑해줘야 한다.
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
